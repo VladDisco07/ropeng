734812d8c55fa627b560dd72ecc788db9f085167
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-26 21:47:48 +0200

    Update peng.js

--- peng.js
+++ peng.js
@@ -1,31 +1,19 @@
-/*
-  A bot that welcomes new guild members when they join
-*/
+//outdated (editing this in 2021)
 
-// Import the discord.js module
 const Discord = require('discord.js');
 
-// Create an instance of a Discord client
 const client = new Discord.Client();
 
-// The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'NDUxODY5MTAzODA4ODM5Njkw.DfIDsA.-rvbUMEpCOohL5t6gZWTpxVfKMM';
+const token = 'no';
 
-// The ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find('name', 'member-log');
-  // Do nothing if the channel wasn't found on this server
   if (!channel) return;
-  // Send the message, mentioning the member
   channel.send(`Bun venit ${member} pe _Club Penguin Romania_! Inainte de a scrie in chat, te rugam sa citesti regulamentul`);
 });
 
-// Log our bot in
 client.login(token);
